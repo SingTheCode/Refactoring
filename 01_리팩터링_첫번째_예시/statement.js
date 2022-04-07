8069a3d64a6690cd8579c8aee7ca2de7f0b3f418
spiderq2697 <spiderq10@gmail.com>
2022-04-07 11:22:24 +0900

    p52: 계산단계와 포맷팅 단계 분리하기
    - 6.1 함수 추출하기
    
    - statement()의 HTML 버전을 만드려고 하는데  계산 함수들이 텍스트버전인 statement() 함수 안에 들어있다
    - 텍스트 버전과 HTML 버전 함수 모두가 똑같은 계산 함수를 사용하게 하고 싶다
    - 6.11 단계 쪼개기를 사용한다. statement()의 로직을 두 단계로 나누는 것이 목표이다
    - 첫번 째 단계에서는 두 번째 단계로 전달할 중간 데이터 구조를 생성

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -1,68 +1,7 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  // 포인트를 적립한다.
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ("comedy" === playFor(aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (playFor(aPerformance).type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-  }
-  return result;
-}
-
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minmumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
-function totalVolumeCredits(invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += volumeCreditsFor(perf);
-  }
-  return result;
-}
-
-function totalAmount(invoice) {
-  let result = 0;
-  for (let perf of invoice.performances) {
-    result += amountFor(perf);
-  }
-  return result;
-}
-
-function statement(invoice, plays) {
+function renderPlainText(invoice, plays) {
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -75,6 +14,71 @@ function statement(invoice, plays) {
   result += `총액: ${usd(totalAmount(invoice))}\n`;
   result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
   return result;
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // 포인트를 적립한다.
+    result += Math.max(aPerformance.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minmumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function totalVolumeCredits(invoice) {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += volumeCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount(invoice) {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
+    return result;
+  }
+}
+
+function statement(invoice, plays) {
+  return renderPlainText(invoice, plays);
 }
 
 console.log(statement(invoices, plays));

902196d843344424e2a42f7c047953198f5898cf
spiderq2697 <spiderq10@gmail.com>
2022-04-07 10:07:47 +0900

    p44: format 변수 제거하기
    - 6.5 함수 선언 바꾸기
    
    - format은 이 함수가 하는 일을 충분히 설명해주지 못한다
    - formatAsUSD라고 하기에는 너무 장황하다
    - 화폐 단위 맞추기가 핵심
    - 처음에는 당장 떠오르는 최선의 이름을 사용하다가, 나중에 더 좋은 이름이 떠오를 때 바꾸는 식이 좋다

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -38,12 +38,12 @@ function amountFor(aPerformance) {
   return result;
 }
 
-function format(aNumber) {
+function usd(aNumber) {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minmumFractionDigits: 2,
-  }).format(aNumber);
+  }).format(aNumber / 100);
 }
 
 function statement(invoice, plays) {
@@ -53,12 +53,12 @@ function statement(invoice, plays) {
   for (let perf of invoice.performances) {
     volumeCredits = volumeCreditsFor(perf);
     //청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
 
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
   }
   return result;

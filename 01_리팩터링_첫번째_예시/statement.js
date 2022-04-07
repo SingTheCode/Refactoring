6cc7b08bf7fe77670de5235d9ffd9f09ffa0d52a
spiderq2697 <spiderq10@gmail.com>
2022-04-07 10:30:18 +0900

    p46: volumeCredits 변수 제거하기
    - 6.1 함수로 추출
    
    volumeCredits 값 갱신과 관련된 문장들을 한데 모아두면 임시 변수를 질의 함수로 바꾸기가 수월해진다.

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -46,7 +46,14 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 
-let volumeCredits = 0;
+function totalVolumeCredits(invoice) {
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
+  }
+  return volumeCredits;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -59,10 +66,7 @@ function statement(invoice, plays) {
     totalAmount += amountFor(perf);
   }
 
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-
+  let volumeCredits = totalVolumeCredits(invoice);
   result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;

f9f5c2e0fb47c8ee6cae6df8c7f9c8cabfa46492
spiderq2697 <spiderq10@gmail.com>
2022-04-07 09:59:48 +0900

    p42: 적립 포인트 계산 코드 추출하기
    - 변수 이름을 적절히 바꾼다

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -1,15 +1,15 @@
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
-function volumeCreditsFor(perf) {
-  let volumeCredits = 0;
+function volumeCreditsFor(aPerformance) {
+  let result = 0;
   // 포인트를 적립한다.
-  volumeCredits += Math.max(perf.audience - 30, 0);
+  result += Math.max(aPerformance.audience - 30, 0);
 
   // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
-  return volumeCredits;
+  if ("comedy" === playFor(aPerformance).type)
+    result += Math.floor(aPerformance.audience / 5);
+  return result;
 }
 
 function playFor(aPerformance) {

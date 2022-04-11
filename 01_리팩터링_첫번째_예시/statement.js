4adabfa31860998e5e5d56a47eec7e389c4e74dc
spiderq2697 <spiderq10@gmail.com>
2022-04-11 15:33:46 +0900

    p60: 계산단계와 포맷팅 단계 분리하기
    - 각 코드를 별도의 파일에 저장한다

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -1,3 +1,5 @@
+const { createStatementData } = require("./createStatementData.js");
+
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
@@ -24,68 +26,6 @@ function renderPlainText(data, plays) {
 
 function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
-}
-
-function enrichPerformance(aPerformance) {
-  const result = Object.assign({}, aPerformance);
-  result.play = playFor(result);
-  result.amount = amountFor(result);
-  result.volumeCredits = volumeCreditsFor(result);
-  return result;
-}
-
-function createStatementData(invoice, plays) {
-  const statementData = {};
-  statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances.map(enrichPerformance);
-  statementData.totalAmount = totalAmount(statementData);
-  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-  return statementData;
-}
-
-function playFor(aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(aPerformance) {
-  let result = 0;
-  switch (aPerformance.play.type) {
-    case "tragedy": // 비극
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-  }
-  return result;
-}
-
-function volumeCreditsFor(aPerformance) {
-  let result = 0;
-  // 포인트를 적립한다.
-  result += Math.max(aPerformance.audience - 30, 0);
-
-  // 희극 관객 5명마다 추가 포인트를 제공한다.
-  if ("comedy" === aPerformance.play.type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function totalVolumeCredits(data) {
-  return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-}
-
-function totalAmount(data) {
-  return data.performances.reduce((total, p) => total + p.amount, 0);
 }
 
 console.log(statement(invoices, plays));

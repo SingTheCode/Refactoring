c2d55a858958c817c81ae4c5435975951f5e7fa2
spiderq2697 <spiderq10@gmail.com>
2022-04-11 21:08:21 +0900

    p70: 공연료 계산기를 다형성 버전으로 만들기
    - 12.6 타입 코드를 서브클래스로 바꾸기
    - 11.8 생성자를 팩터리 함수로 바꾸기

--- 01_리팩터링_첫번째_예시/createStatementData.js
+++ 01_리팩터링_첫번째_예시/createStatementData.js
@@ -83,6 +83,21 @@ function createStatementData(invoice, plays) {
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy":
+      return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy":
+      return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르 ${aPlay.type}`);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {}
+
+class ComedyCalculator extends PerformanceCalculator {}
+
 module.exports = {
   createStatementData,
 };

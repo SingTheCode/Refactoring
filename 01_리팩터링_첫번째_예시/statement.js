aacc68c202001bab651642edc4a8e220d00f1fea
spiderq2697 <spiderq10@gmail.com>
2022-04-11 14:48:53 +0900

    p58: 계산단계와 포맷팅 단계 분리하기
    - 총합을 구하는 부분을 옮긴다

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -9,8 +9,8 @@ function renderPlainText(data, plays) {
     result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
   }
 
-  result += `총액: ${usd(totalAmount())}\n`;
-  result += `적립 포인트: ${totalVolumeCredits()}점\n`;
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
   return result;
 
   function usd(aNumber) {
@@ -20,28 +20,14 @@ function renderPlainText(data, plays) {
       minmumFractionDigits: 2,
     }).format(aNumber / 100);
   }
-
-  function totalVolumeCredits() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
-  }
-
-  function totalAmount() {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
-  }
 }
 
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
   statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
   return renderPlainText(statementData, plays);
 
   function enrichPerformance(aPerformance) {
@@ -86,6 +72,22 @@ function statement(invoice, plays) {
     // 희극 관객 5명마다 추가 포인트를 제공한다.
     if ("comedy" === aPerformance.play.type)
       result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function totalVolumeCredits(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.volumeCredits;
+    }
+    return result;
+  }
+
+  function totalAmount(data) {
+    let result = 0;
+    for (let perf of data.performances) {
+      result += perf.amount;
+    }
     return result;
   }
 }

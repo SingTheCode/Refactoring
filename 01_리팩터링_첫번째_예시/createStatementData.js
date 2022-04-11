61168353cb0093de0391a32e1538b4a275772dca
spiderq2697 <spiderq10@gmail.com>
2022-04-11 16:56:38 +0900

    p67: 다형성을 활용해 계산 코드 재구성하기
    - 6.5 함수 선언 바꾸기
    - 공연할 연극을 계산기로 전달한다

--- 01_리팩터링_첫번째_예시/createStatementData.js
+++ 01_리팩터링_첫번째_예시/createStatementData.js
@@ -1,6 +1,7 @@
 class PerformanceCalculator {
-  constructor(aPerformance) {
+  constructor(aPerformance, aPlay) {
     this.performance = aPerformance;
+    this.play = aPlay;
   }
 }
 
@@ -13,7 +14,7 @@ function createStatementData(invoice, plays) {
   return statementData;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance);
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);

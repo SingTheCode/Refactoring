5527f21f75b739ce21b09565c09b37a75466490e
spiderq2697 <spiderq10@gmail.com>
2022-04-11 15:40:25 +0900

    p60: 계산단계와 포맷팅 단계 분리하기
    - rederHTML() 함수 만들기

--- 01_리팩터링_첫번째_예시/statement.js
+++ 01_리팩터링_첫번째_예시/statement.js
@@ -3,29 +3,30 @@ const { createStatementData } = require("./createStatementData.js");
 const invoices = require("./invoices.json");
 const plays = require("./plays.json");
 
-function renderPlainText(data, plays) {
-  let result = `청구 내역 (고객명: ${data.customer})\n`;
-
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
   for (let perf of data.performances) {
-    //청구 내역을 출력한다.
-    result += `${perf.play.name}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+    result += `<tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+    result += `<td>${usd(perf.amount)} </td></tr>\n`;
   }
-
-  result += `총액: ${usd(data.totalAmount)}\n`;
-  result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
   return result;
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minmumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 }
 
-function statement(invoice, plays) {
-  return renderPlainText(createStatementData(invoice, plays));
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minmumFractionDigits: 2,
+  }).format(aNumber / 100);
 }
 
-console.log(statement(invoices, plays));
+function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
+console.log(htmlStatement(invoices, plays));

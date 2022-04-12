2419742d8ee6f3f44c7fd8d004650f97fa97f6cf
spiderq2697 <spiderq10@gmail.com>
2022-04-12 17:40:36 +0900

    p73: 공연료 계산기를 다형성 버전으로 만들기
    - 일반적인 경우를 기본으로 삼아 슈퍼클래스에 남겨두고, 장르마다 달라지는 부분은 필요할 때 오버라이드하게 만드는 것이 좋다

--- 01_리팩터링_첫번째_예시/createStatementData.js
+++ 01_리팩터링_첫번째_예시/createStatementData.js
@@ -71,6 +71,10 @@ class TragedyCalculator extends PerformanceCalculator {
     }
     return result;
   }
+
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30);
+  }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
@@ -81,6 +85,10 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 

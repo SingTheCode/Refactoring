729847eede4f9b6ffa9d87eb14c22d0e6c0ab56e
spiderq2697 <spiderq10@gmail.com>
2022-04-14 14:04:43 +0900

    p149: 경계조건 검사하기
    - 검증 단계에서 실제 값이 예상범위를 벗어나면 실패를 띄운다
    - 검증보다 앞선 과정에서 예외 상황이 발생하면 에러를 띄운다

--- 02_테스트_구축하기/test.js
+++ 02_테스트_구축하기/test.js
@@ -16,4 +16,51 @@ describe("province", () => {
     expect(asia.shortfall).toBe(-6);
     expect(asia.profit).toBe(292);
   });
+  test("zero demand", () => {
+    asia.demand = 0;
+    expect(asia.shortfall).toBe(-25);
+    expect(asia.profit).toBe(0);
+  });
+  test("negative demand", () => {
+    asia.demand = -1;
+    expect(asia.shortfall).toBe(-26);
+    expect(asia.profit).toBe(-10);
+  });
+  test("empty string demand", () => {
+    asia.demand = "";
+    expect(asia.shorfall).NaN;
+    expect(asia.profit).NaN;
+  });
+});
+
+describe("no producers", () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: "No producers",
+      producers: [],
+      demand: 30,
+      price: 20,
+    };
+    noProducers = new Province(data);
+  });
+  test("shortfall", () => {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  test("profit", () => {
+    expect(noProducers.profit).toBe(0);
+  });
+});
+
+describe("string for producers", () => {
+  test("", () => {
+    const data = {
+      name: "String producers",
+      producers: "",
+      demand: 30,
+      price: 20,
+    };
+    const prov = new Province(data);
+    expect(prov.shortfall).equal(0);
+  });
 });

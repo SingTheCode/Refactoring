ae276b75087d4678bf0d83149dbc9508dc3d1bd3
spiderq2697 <spiderq10@gmail.com>
2022-04-14 13:27:16 +0900

    fix: production() 세터를 수정한다

--- 02_테스트_구축하기/index.js
+++ 02_테스트_구축하기/index.js
@@ -115,7 +115,7 @@ class Producer {
   set production(amountStr) {
     const amount = parseInt(amountStr);
     const newProduction = Number.isNaN(amount) ? 0 : amount;
-    this._province.totalProduction += newProduction = this._production;
+    this._province.totalProduction += newProduction - this._production;
     this._production = newProduction;
   }
 }

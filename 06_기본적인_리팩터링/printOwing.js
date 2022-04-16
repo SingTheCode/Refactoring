a29d96e2a14ef4b88883249cdd3a64aa18c0215d
spiderq2697 <spiderq10@gmail.com>
2022-04-17 00:29:31 +0900

    p162: 유효범위를 벗어나는 변수가 없을 때
    - 세부사항을 출력하는 코드를 간단히 추출한다

--- 06_기본적인_리팩터링/printOwing.js
+++ 06_기본적인_리팩터링/printOwing.js
@@ -16,10 +16,14 @@ function printPwing(invoice) {
     today.getDate() + 30
   );
 
-  // 세부 사항을 출력한다.
-  console.log(`고객명: ${invoice.customer}`);
-  console.log(`채무액: ${outstanding}`);
-  console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  printDetails();
+
+  function printDetails() {
+    // 세부 사항을 출력한다.
+    console.log(`고객명: ${invoice.customer}`);
+    console.log(`채무액: ${outstanding}`);
+    console.log(`마감일: ${invoice.dueDate.toLocaleDateString()}`);
+  }
 }
 
 function printBanner() {

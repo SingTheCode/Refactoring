a156bffaed4d15359d2142e43298ef08f9ef0ee7
spiderq2697 <spiderq10@gmail.com>
2022-04-17 13:49:09 +0900

    p170: 6.2 함수 인라인 하기
    - 호출되는 함수의 반환문을 그대로 복사해서 호출하는 함수의 호출문을 덮어쓴다

--- 06_기본적인_리팩터링/rating.js
+++ 06_기본적인_리팩터링/rating.js
@@ -1,7 +1,3 @@
 function rating(aDriver) {
-  return moreThenFiveLateDeliveries(aDriver) ? 2 : 1;
-}
-
-function moreThenFiveLateDeliveries(aDriver) {
-  return aDriver.numberOfLateDeliveries > 5;
+  return aDriver.numberOfLateDeliveries > 5 ? 2 : 1;
 }

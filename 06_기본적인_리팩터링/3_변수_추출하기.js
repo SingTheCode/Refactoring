bc52a2b91afff2ab2edd69edf9ebe7f27415c93a
spiderq2697 <spiderq10@gmail.com>
2022-04-18 13:13:22 +0900

    p176: 6.3 변수 추출하기
    - 똑같은 코드를 클래스 문맥 안에서 처리하는 방법

--- 06_기본적인_리팩터링/3_변수_추출하기.js
+++ 06_기본적인_리팩터링/3_변수_추출하기.js
@@ -1,7 +1,31 @@
+// 예시
 function price(order) {
   const basePrice = order.quantity * order.itemPrice;
   const quantityDiscount =
     Math.max(0, order.quantity - 500) * order.itemPrice * 0.05;
-  const shipping = Math.min(basePrice * 0.1, 100)
+  const shipping = Math.min(basePrice * 0.1, 100);
   return basePrice - quantityDiscount + shipping;
+}
+
+// 클래스 안에서
+class Order {
+  constructor(aRecord) {
+    this._data = aRecord;
+  }
+
+  get quantity() {
+    return this._data.quantity;
+  }
+
+  get itemPrice() {
+    return this._data.itemPrice;
+  }
+
+  get price() {
+    return (
+      this.quantity * this.itemPrice -
+      Math.max(0, this.quantity - 500) * this.itemPrice * 0.05 +
+      Math.min(this.quantity * this.itemPrice * 0.1, 100)
+    );
+  }
 }

8acbe5a354f5fa4969a2b5913ba6c9140bccacc3
spiderq2697 <spiderq10@gmail.com>
2022-04-19 16:09:58 +0900

    p190: 변수 캡슐화하기
    - get, set 함수 정의

--- 06_기본적인_리팩터링/6_변수_캡슐화하기.js
+++ 06_기본적인_리팩터링/6_변수_캡슐화하기.js
@@ -1,5 +1,13 @@
 // 예시
 let defaultOwner = { firstName: "마틴", lastName: "파울러" };
 
+function getDefaultOwner() {
+  return defaultOwner;
+}
+
+function setDefaultOwner(arg) {
+  defaultOwner = arg;
+}
+
 spaceship.owner = defaultOwner;
 defaultOwner = { firstName: "레베카", lastName: "파슨스" };

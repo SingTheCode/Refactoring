c3c61f3ffbbb65787029fefaa69b6bf455e2a212
spiderq2697 <spiderq10@gmail.com>
2022-04-19 15:07:02 +0900

    p187: 6.5 함수 선언 바꾸기
    - 6.2 함수 인라인하기

--- 06_기본적인_리팩터링/5_함수_선언_바꾸기.js
+++ 06_기본적인_리팩터링/5_함수_선언_바꾸기.js
@@ -29,8 +29,4 @@ function inNewEngland(aCustomer) {
   return xxNEWinNewEngland(aCustomer.address.state);
 }
 
-function xxNEWinNewEngland(stateCode) {
-  return ["MA", "CT", "ME", "VT", "NH", "RI"].includes(stateCode);
-}
-
-const newEnglanders = someCustomers.filter((c) => inNewEngland(c));
+const newEnglanders = someCustomers.filter((c) => inNewEngland(c.address.state));

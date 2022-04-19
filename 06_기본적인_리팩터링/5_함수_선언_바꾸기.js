f2dda4bfe8f251b3401ba05c3e4d02387ae89bb4
spiderq2697 <spiderq10@gmail.com>
2022-04-19 14:50:49 +0900

    p184: 6.5 함수 선언 바꾸기
    - 원하는 매개변수를 추가한다

--- 06_기본적인_리팩터링/5_함수_선언_바꾸기.js
+++ 06_기본적인_리팩터링/5_함수_선언_바꾸기.js
@@ -15,10 +15,10 @@ function circumference(radius) {
 // 매개변수 추가하기
 class Book {
   addReservation(customer) {
-    this.zz_addReservation(customer);
+    this.zz_addReservation(customer, false);
   }
 
-  zz_addReservation(customer) {
+  zz_addReservation(customer, isPriority) {
     this._reservations.push(customer);
   }
 }

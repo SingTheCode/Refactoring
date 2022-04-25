e6026d99bd505f0058201c9150eb35557b0ca764
spiderq2697 <spiderq10@gmail.com>
2022-04-25 11:38:39 +0900

    p205: calculateBaseCharge()를 옮긴다

--- 06_기본적인_리팩터링/9_여러_함수를_클래스로_묶기.js
+++ 06_기본적인_리팩터링/9_여러_함수를_클래스로_묶기.js
@@ -2,7 +2,8 @@
 reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
 // 클라이언트1
-const aReading = acquireReading();
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
 const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
 
 // 클라이언트2

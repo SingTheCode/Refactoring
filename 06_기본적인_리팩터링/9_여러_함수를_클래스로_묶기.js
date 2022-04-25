5493cce13b61baca457713f258a33ae1c6e46849
spiderq2697 <spiderq10@gmail.com>
2022-04-25 11:33:58 +0900

    p203: 예시
    - 매달 차 계량기를 읽어서 측정값을 기록한다고 하자

---
+++ 06_기본적인_리팩터링/9_여러_함수를_클래스로_묶기.js
@@ -0,0 +1,17 @@
+// 예시
+reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
+
+// 클라이언트1
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// 클라이언트2
+const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+// 클라이언트3
+const basicChargeAmount = calculateBaseCharge(aReading);
+
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}

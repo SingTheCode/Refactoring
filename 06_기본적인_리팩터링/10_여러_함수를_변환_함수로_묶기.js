fcb8a606a01a4ffad60d449bacd496975b404ed0
spiderq2697 <spiderq10@gmail.com>
2022-04-26 13:02:02 +0900

    p211: calculateBaseCharge()를 부가 정보를 덧붙이는 코드 근처로 옮긴다

--- 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
+++ 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
@@ -19,5 +19,6 @@ function calculateBaseCharge(aReading) {
 
 function enrichReading(original) {
   const result = _.cloneDeep(original);
+  result.baseCharge = calculateBaseCharge(result);
   return result;
 }

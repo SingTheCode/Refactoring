c4095b71f3c2b436afc8c68fcd61b0c65b4b2560
spiderq2697 <spiderq10@gmail.com>
2022-04-26 13:45:48 +0900

    p213: 계산 코드를 변환 함수로 옮긴다

--- 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
+++ 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
@@ -21,5 +21,9 @@ function calculateBaseCharge(aReading) {
 function enrichReading(original) {
   const result = _.cloneDeep(original);
   result.baseCharge = calculateBaseCharge(result);
+  result.taxableCharge = Math.max(
+    0,
+    result.baseCharge - taxThreshold(result.year)
+  );
   return result;
 }

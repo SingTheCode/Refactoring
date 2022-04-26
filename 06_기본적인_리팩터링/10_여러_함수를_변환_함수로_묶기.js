7b5a6f45fe7aca4a6d900bb73f8495ba7d9f7eb5
spiderq2697 <spiderq10@gmail.com>
2022-04-26 13:40:22 +0900

    p212: 클라이언트 1도 이 필드를 사용하도록 수정한다

--- 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
+++ 06_기본적인_리팩터링/10_여러_함수를_변환_함수로_묶기.js
@@ -4,8 +4,7 @@ reading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 // client1
 const rawReading = acquireReading();
 const aReading = enrichReading(rawReading);
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
-
+const baseCharge = aReading.baseCharge;
 // client2
 const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
 const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
